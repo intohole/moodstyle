a70bc8aed97736dc4e9383843e177c1b98dbdabe
lixuze <intoblack86@gmail.com>
2013-11-27 23:19:26 +0800

    添加互信息 方法

--- text-feature/PTextFeatureExtract.py
+++ text-feature/PTextFeatureExtract.py
@@ -2,6 +2,10 @@
 # coding:utf-8
 
 
+import math
+import operator
+
+
 class Document(object):
 
     """
@@ -45,86 +49,99 @@ class Document(object):
 
     def __insert_doc_dict(self, doc_type, word):
         __value = 1
-        if self._doc.has_key(word):
-            if self._doc[word].has_key(doc_type):
-                __value = self._doc[word][doc_type] + 1
+        if self.__doc.has_key(word):
+            if self.__doc[word].has_key(doc_type):
+                __value = self.__doc[word][doc_type] + 1
         else:
-            self._doc[word] = {}
-        self._doc[word][doc_type] = __value
+            self.__doc[word] = {}
+        self.__doc[word][doc_type] = __value
 
-
-    def get_doc_count(self , doc_type):
-    	if self.__type_count.has_key(doc_type):
-    		return self.__type_count[doc_type]
-    	return 0
+    def get_doc_count(self, doc_type):
+        if self.__type_count.has_key(doc_type):
+            return self.__type_count[doc_type]
+        return 0
 
     def get_word_set(self):
-    	return self.__doc.keys()
+        return self.__doc.keys()
 
     def get_type_set(self):
-    	return self.__type_count.keys()
-
+        return self.__type_count.keys()
 
 
 class ITextFeatureScore(object):
 
- 	'''
- 	doc_word_count , 特定文档中出现该词的文档数目
- 	doc_count , 特定类别的文档数目
- 	word_count , 特定存在文档总数目
- 	doc_sum , 文档总数目
- 	功能： 计算 每个词的分值
- 	返回: double
+    '''
+    doc_word_count , 特定文档中出现该词的文档数目
+    doc_count , 特定类别的文档数目
+    word_count , 特定存在文档总数目
+    doc_sum , 文档总数目
+    功能： 计算 每个词的分值
+    返回: double
 
- 	'''
-	def feature_socre(self , doc_word_count , doc_count , word_count , doc_sum):
-		raise NotImplementedError
+    '''
 
-class IM(ITextFeatureScore):
-		
-	def feature_socre(self , doc_word_count , doc_count , word_count , doc_sum):
-		return 0.
+    def feature_socre(self, doc_word_count, doc_count, word_count, doc_sum):
+        raise NotImplementedError
+
+
+# class IM(ITextFeatureScore):
+
+#     def feature_socre(self, doc_word_count, doc_count, word_count, doc_sum):
+#         return math.log(float(doc_sum * doc_word_count) / float(doc_count * word_count))
+
 
 class TextFeature(object):
 
+    def __init__(self,  filter_rate=0.001):
+        # if text_feature_score and hasattr(text_feature_score,'feature_socre' ):
+        #     raise TypeError
+        self.filter_rate = filter_rate
 
-	def __init__(self ,text_feature_score  , filter_rate = 0.0004 ):
-		if text_feature_score and isinstance(text_feature_score , ITextFeatureScore):
-			raise TypeError
-		self.filter_rate = filter_rate
-		self.text_feature_score = text_feature_score
-    
-	def insert_document_list(self , contents):
-		doc = Document()
-		for line in contents:
-			lineInfo = line.split('\t')
-			if len(lineInfo) >=2 :
-				word_set = set(lineInfo[1].split(" "))
-				doc.insert_document(lineInfo[0] , word_set)
-		return doc
+    def insert_document_list(self, contents):
+        doc = Document()
+        for line in contents:
+            lineInfo = line.split('\t')
+            if len(lineInfo) >= 2:
+                word_set = set(lineInfo[1].split(" "))
+                doc.insert_document(lineInfo[0], word_set)
+        return doc
+
+    def extract_feature_from_contents(self, top_word, contents):
+        doc = self.insert_document_list(contents)
+        doc_word_score_map = {}
+        for doc_type in doc.get_type_set():
+            doc_word_score_map[doc_type] = {}
+        for word in doc.get_word_set():
+            for doc_type in doc.get_type_set():
+                if doc.get_type_word_count(doc_type, word) < long(doc.get_doc_count(doc_type) * self.filter_rate):
+                    continue
+                score = self.text_feature_score(doc.get_type_word_count(
+                    doc_type, word), doc.get_doc_count(doc_type), doc.get_word_count(word), doc.doc_count)
+                doc_word_score_map[doc_type][word] = score
+        for doc_type, word_score in doc_word_score_map.items():
+            sorted_x = sorted(
+                word_score.iteritems(),  key=lambda x : x[1], reverse=True)
+            doc_word_score_map[doc_type] = [word[0] for word in sorted_x]
+        return doc_word_score_map
 
 
-	def extract_feature_from_contents(self , top_word ,contents):
-		doc = self.insert_document_list(contents)
-		doc_word_score_map = {}
-		for doc_type in doc.get_type_set():
-			doc_word_score_map[doc_type] = {}
-		for word in doc.get_word_set():
-			for doc_type in doc.get_type_set():
-				if doc.get_type_word_count(doc_type , word) < long(doc.get_doc_count(doc_type) * self.filter_rate):
-					continue
-				score = self.text_feature_score(doc.get_type_word_count(doc_type , word) , doc.get_doc_count(doc_type) , doc.get_word_count(word) , doc.doc_count)
-				doc_word_score_map[doc_type][word] = score
+    def text_feature_score(self, doc_word_count, doc_count, word_count, doc_sum):
+    	pass
 
 
+class IM(TextFeature):
+ 
 
-
-
+ 	def text_feature_score(self, doc_word_count, doc_count, word_count, doc_sum):
+ 	    return math.log(float(doc_sum * doc_word_count) / float(doc_count * word_count))
+ 		
 
 
 if __name__ == '__main__':
-	s = TextFeature(IM)
-
-
-
-
+    s = IM()
+    contents = []
+    with open('/home/lixuze/fenlei/type_file.txt') as f:
+        contents = [line.strip() for line in f.readlines()]
+    for doc_type , word_list in s.extract_feature_from_contents(100, contents).items():
+    	print doc_type
+    	print ' '.join(word_list)
