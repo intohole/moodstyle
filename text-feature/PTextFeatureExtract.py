69ae210642408d823cf830cff07efa4befa075ba
lixuze <intoblack86@gmail.com>
2013-11-29 23:55:24 +0800

    添加df 计算方法

--- text-feature/PTextFeatureExtract.py
+++ text-feature/PTextFeatureExtract.py
@@ -24,7 +24,8 @@ class Document(object):
             self.__insert_doc_dict(doc_type, word)
         self.__insert_type_dict(doc_type)
         self.doc_count = self.doc_count + 1
-    #获得word在所有文档的总数量
+    # 获得word在所有文档的总数量
+
     def get_word_count(self, word):
         if not (word and isinstance(word, (str, unicode))):
             raise TypeError, 'word must be is str or unicode'
@@ -86,7 +87,8 @@ class ITextFeatureScore(object):
 # class IM(ITextFeatureScore):
 
 #     def feature_socre(self, doc_word_count, doc_count, word_count, doc_sum):
-#         return math.log(float(doc_sum * doc_word_count) / float(doc_count * word_count))
+# return math.log(float(doc_sum * doc_word_count) / float(doc_count *
+# word_count))
 
 
 class TextFeature(object):
@@ -107,55 +109,77 @@ class TextFeature(object):
 
     def extract_feature_from_contents(self, top_word, contents):
         doc = self.insert_document_list(contents)
-        doc_word_score_map = {} #文档 ——》 词 ——》分值
-        for doc_type in doc.get_type_set(): #初始化 分值dict 这样不用下面判断
-            doc_word_score_map[doc_type] = {} 
+        doc_word_score_map = {}  # 文档 ——》 词 ——》分值
+        for doc_type in doc.get_type_set():  # 初始化 分值dict 这样不用下面判断
+            doc_word_score_map[doc_type] = {}
         for word in doc.get_word_set():
             for doc_type in doc.get_type_set():
-                if doc.get_type_word_count(doc_type, word) < long(doc.get_doc_count(doc_type) * self.filter_rate): #现在简单的滤除低词频
+                #现在简单的滤除低词频
+                if doc.get_type_word_count(doc_type, word) < long(doc.get_doc_count(doc_type) * self.filter_rate):
                     continue
                 score = self.text_feature_score(doc.get_type_word_count(
                     doc_type, word), doc.get_doc_count(doc_type), doc.get_word_count(word), doc.doc_count)
                 doc_word_score_map[doc_type][word] = score
-        for doc_type, word_score in doc_word_score_map.items(): #对所有按照分值大小排序
+        for doc_type, word_score in doc_word_score_map.items():  # 对所有按照分值大小排序
             sorted_x = sorted(
-                word_score.iteritems(),  key=lambda x : x[1], reverse=True)
-            doc_word_score_map[doc_type] = [word[0] for word in sorted_x][0:top_word]
+                word_score.iteritems(),  key=lambda x: x[1], reverse=True)
+            doc_word_score_map[doc_type] = [word[0]
+                                            for word in sorted_x][0:top_word]
         return doc_word_score_map
 
-
     def text_feature_score(self, doc_word_count, doc_count, word_count, doc_sum):
-    	'''
-    	子类必须要实现的方法  对每个打分 
-    	'''
-    	pass
+        '''
+        子类必须要实现的方法  对每个打分
+        '''
+        pass
 
 
 class IM(TextFeature):
-	'''
-	互信息 方法计算 ——》 香浓熵 
 
-	'''
- 	def text_feature_score(self, doc_word_count, doc_count, word_count, doc_sum):
- 	    return math.log(float(doc_sum * doc_word_count) / float(doc_count * word_count))
+    '''
+    互信息 方法计算 ——》 香浓熵
+
+    '''
+
+    def text_feature_score(self, doc_word_count, doc_count, word_count, doc_sum):
+        return math.log(float(doc_sum * doc_word_count) / float(doc_count * word_count))
 
 
 class CHI(TextFeature):
 
-	def text_feature_score(self, doc_word_count, doc_count, word_count, doc_sum):
-		__A = doc_word_count 
-		__B = word_count - doc_word_count
-		__C = doc_count - doc_word_count
-		__D = doc_sum - (word_count  + doc_count - doc_word_count )
-		return (float((__A * __D - __B * __C) * (__A * __D - __B * __C)) / float(word_count * (doc_sum - word_count)))
-		
+    def text_feature_score(self, doc_word_count, doc_count, word_count, doc_sum):
+        __A = doc_word_count
+        __B = word_count - doc_word_count
+        __C = doc_count - doc_word_count
+        __D = doc_sum - (word_count + doc_count - doc_word_count)
+        return (float((__A * __D - __B * __C) * (__A * __D - __B * __C)) / float(word_count * (doc_sum - word_count)))
+
+
+class DF(TextFeature):
+
+    '''
+    文档率 计算 ===》 简单理解就是一个 去除高词频
+    去除低词频 算法简单
+    '''
+    MAX_FILTER = 0.006
+
+    def text_feature_score(self, doc_word_count, doc_count, word_count, doc_sum):
+        if float(doc_word_count) / float(doc_count) > self.MAX_FILTER: #去除一定的高词频
+            return 0.
+        return float(doc_word_count) / float(doc_count)
+
+
+class IG(TextFeature):
+    #信息增益
+
+    pass
 
 
 if __name__ == '__main__':
-    s = CHI()
+    s = DF()
     contents = []
     with open('/home/lixuze/fenlei/type_file.txt') as f:
         contents = [line.strip() for line in f.readlines()]
-    for doc_type , word_list in s.extract_feature_from_contents(100, contents).items():
-    	print doc_type
-    	print ' '.join(word_list)
+    for doc_type, word_list in s.extract_feature_from_contents(100, contents).items():
+        print doc_type
+        print ' '.join(word_list)
