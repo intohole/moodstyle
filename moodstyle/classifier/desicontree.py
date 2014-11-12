a8604119c3e403ee453132ab3cda9f5615e015bb
intoblack <intoblack86@gmail.com>
2014-11-12 23:35:06 +0800

    文档相似度代码　重新修改

--- moodstyle/classifier/desicontree.py
+++ moodstyle/classifier/desicontree.py
@@ -2,83 +2,50 @@
 #!/usr/bin/env python
 
 import math
-
-
-
-
-
-
 from collections import defaultdict
-class DefaultDecisionTree(object) :
-    '''
-    二分类决策树实现思路　
-    找到一个信息增益最高的属性　
-    按照属性进行切分数据　
-    '''
+
+
+
+
+class TrainDT(object):
 
     def __init__(self):
         pass
 
 
+    def train(self , data , attrs ):
+        '''
+        data -> [label , data1 , data2 , ....., dataN]
+        attrs -> [attr1 , attr2 , attr3 ,.....,attrN]
+        '''
+        dt = {}
 
-    def predict(self , data):
+
+
+
+    def split_data(self , data , attr_name , attr_value , attrs):
+        '''
+        split classifier data
+        '''
+        attr_index = attrs.index(attr_name)
+        __attrs = attrs[:attr_index]
+        __attrs.extend(attrs[attr_index+1:])
+        __data = []
+        attr_index += 1 # label in the every data first index 
+        for dd in data:
+            if dd[attr_index] == attr_value:
+                d = dd[:attr_index]
+                d.extend(attrs[attr_index + 1 :])
+                __data.append(d)
+        return __data , __cattrs
+
+    def find_best_attr(self , data , attrs):
         pass
-
-
-
-    def train(self , datas):
-        pass
-
-
-
-    def split_datas_by_attr(self , datas , attr , attsed ):
-        '''
-        将数据按照某个属性进行split动作　，　决策树计算流程　，　按照某个属性对系统的增益最大（最能确定分类元素）　，
-        建立节点按照
-        '''
-        tree_root = {}
-
-
-
-    def entropy(self , prob ):
-        if prob and isinstance(prob, float) and prob >= 0. and prob <= 1.:
-            return (-prob) * math.log(prob, 2)
-
-
-    def calc_info_gain(self , datas , attrs ):
-        '''
-        数据形如　：　[{'lable' : 1  , 'data' : [1 , 2 , 3]]
-        attrsed set类型　为已经计算完的属性名称
-        '''
-        data_status = defaultdict(int) # label - > count 
-        data_count = 0
-        for data in datas:
-            data_status[data['label']] += 1
-            data_count += 1
-        sys_info_gain =  0. #系统总信息熵
-        for __label , __count in data_status:
-            sys_info_gain += self.entropy(float(__count) / data_count)
-        attr_label_count = dict() # attr_name - > attr_value - > label_count 
-        for __key in attrs.keys():
-            attr_label_count[__key] = {}
-        #循环数据　　，　计算每个属性
-        #计算每个属性值的时候　分别计算
-        for data in datas: #循环每个数组
-            __data = data['data']
-            __label = data['label']
-            for i in range(__data):
-                if not attr_label_count[attrs[i]].has_key(__data[i]):
-                    attr_label_count[attrs[i]][__data[i]] = {}
-                    for label in data_status.keys():
-                        attr_label_count[attrs[i]][__data[i]][label] = 0 
-                attr_label_count[attrs[i]][__data[i]][__label] += 1    
 
 
 if __name__ == '__main__':
-    x = DefaultDecisionTree()
-    atrrs = {1 : '1' , 2 : '2' , 3: '3'}
-    datas = [{'label' : 0 , 'data' : {'1' : 0 , '2' : 0 , '3' : 0 }} , {'label' : 1 , 'data' : {'1' : 1 , '2' : 2 , '3' : 0 }} ]
-    x.calc_info_gain(datas , set(['2','3']))
-    print x.entropy(9.0 / 14) + x.entropy(5.0 / 14)
-    print x.entropy(6.0 / 9) + x.entropy( 2.0 /5)
-    print x.entropy(3.0 / 9) + x.entropy( 3.0 /5)
+    t = TrainDT()
+    attrs = [ 'a' , 'b' , 'c' ,'d' ,'e']
+    print t.split_data(1 , 'e', 3, attrs)
+    print attrs
+
