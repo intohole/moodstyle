417fb22e30c9452caabbde81c4140f4b6a5e4a88
intoblack <intoblack86@gmail.com>
2014-12-02 21:15:30 +0800

    编写决策树查找算法

--- test/testDecisionTree.py
+++ test/testDecisionTree.py
@@ -2,28 +2,77 @@
 
 
 from collections import defaultdict
+from collections import Counter
 from math import log
 
 
 class Node(object):
 
-    def __init__(self, attr):
-        pass
+    def __init__(self, attr_name=None, label=None):
+        self.attr_name = attr_name
+        self.label = label
+        self.child = {}
+
+    def __str__(self):
+        return 'attr: %s \t label : %s \t ' % (self.attr_name, self.label)
 
 
 class DecisionTree(object):
 
-    def train(self, datas, attrs, denseData=False):
-        labels = [data[-1] for data in datas]
-        if len(set(labels)) <= 1:
-            return {'all': labels[1]}
-        # return self.__getBestFeature(datas , denseData)
+    def __init__(self):
+        self.tree = None
+        self.attrs = None
 
-        # for data , attrs ,
+    def train(self, datas, attrs, threshold=0.01, denseData=True, tree=None):
+        if self.attrs == None:
+            self.attrs = attrs
+        if tree == None:
+            tree = Node()
+            self.tree = tree
+        label_dict = Counter([data[-1] for data in datas])
+        if len(label_dict.keys()) == 1:
+            tree.label = datas[0][-1]
+            return tree  # 如果都输于同一类 ， 则返回树
+            # return Node(label=datas[0][-1])
+        if len(attrs) == 0:
+            tree.label = label_dict.most_common()[0][0]
+            return tree  # 如果属性为空 ， 则返回绝大数的类标记
+            # return Node(label=label_dict.most_common()[0][0])
+        attr, attr_gain, attr_val = self.getBestFeature(
+            datas, attrs, denseData)[0]  # 得到最好信息增益的属性
+        if attr_gain < threshold:
+            tree = label_dict.most_common()[0][0]
+            return tree
+        for val in attr_val:
+            tree.attr_name = attr
+            node = Node()
+            tree.child[val] = node
+            # 暂时还没有修正树的迭代问题 ！！！！
+            self.train(
+                self.splitDataByAttr(
+                    datas, attrs, attr, val
+                ),
+                self.getSplitAttrs(
+                    attrs, attr
+                ),
+                threshold,
+                denseData, node)
+        return tree
 
     @staticmethod
     def entropy(probs):
-        return sum([-prob * log(prob, 2) for prob in probs])
+        if probs:
+            if isinstance(probs, (list, tuple)):
+                return sum([-prob * log(prob, 2) for prob in probs])
+            elif isinstance(probs, (int, float)):
+                return -probs * log(probs, 2)
+
+    def getSplitAttrs(self, attrs, attr):
+        split_attrs = []
+        index = attrs.index(attr)
+        split_attrs.extend(attrs[:index])
+        split_attrs.extend(attrs[index + 1:])
+        return split_attrs
 
     def getBestFeature(self, datas, attrs, denseData):
         '''
@@ -62,6 +111,7 @@ class DecisionTree(object):
         #     H(D) - H(D|A)
         #     系统熵 - 每个属性下 ， 存在这个类别的信息熵
         #
+        # gains = [(属性名称 ， 信息增益 ， (属性值))......(属性名称n , 信息增益n ， (f1 ...fn))]
         gains = [(attr,
                   label_entropy -
                   sum(
@@ -69,37 +119,62 @@ class DecisionTree(object):
                           attr_value_count[attr][value] / data_num *
                           DecisionTree.entropy(
                               [
-                                  #计算每个属性在特定属性值时 ， 发生的概率
+                                  # 计算每个属性在特定属性值时 ， 发生的概率
                                   # p(DA1)/A
                                   attr_value_class_count[attr][value][
                                       label] / attr_value_count[attr][value]
-                                  #循环每个属性值在特定label产生
+                                  # 循环每个属性值在特定label产生
                                   for label in attr_value_class_count[attr][value].keys()
                               ]
                           )
                           for value in attr_value_count[attr].values() if attr_value_class_count[attr].has_key(value)]
                   ),
-                  attr_value_count[attr].keys() )
+                  attr_value_count[attr].keys())
                  for attr in attr_value_count.keys()]
         return sorted(gains, key=lambda x: x[1], reverse=True)
 
-    def splitDataByAttr(self, datas, attr, atrr_value, denseData=True):
+    def splitDataByAttr(self, datas, attrs, attr_name, attr_value, denseData=True):
+        '''
+        切割训练集为了下一步
+        datas :训练的数据 [[data]]
+        attrs 属性名称列表
+        attr_val 属性值
+        denseData 是否是密集型数据 , 暂时废弃
+        '''
         dump_datas = []
+        index = attrs.index(attr_name)
         for data in datas:
             dump = []
-            if data[attr] == attr_value:
-                dump = data[:attr]
-                dump.extend(data[attr:])
+            if data[index] == attr_value:
+                dump = data[:index]
+                dump.extend(data[index + 1:])
                 dump_datas.append(dump)
         return dump_datas
 
+    def classify(self, data):
+        '''
+        功能： 用于分类模型
+        参数 ：
+            data 待分析的数据 ， list
+        返回:
+            返回决策树的label
+        '''
+        if self.tree == None:
+            raise Exception, 'no model !'
+        node = self.tree
+        if node.label != None:
+            return node.label
+        for _ in range(len(data)):
+            index = self.attrs.index(node.attr_name)
+            node = self.tree.child[data[index]]
+            if node.label != None:
+                return node.label
+        return None
 
 if __name__ == '__main__':
     # 测试数据
     # 是否必须水里 是否有脚蹼 属于鱼类
     data = [[1, 1, 1], [1, 1, 1], [1, 0, 0], [0, 1, 0], [0, 1, 0]]
     d = DecisionTree()
-    # d.train(data)
-    print DecisionTree.entropy([0.333, 0.21])
-    test = [1, 2, 3]
-    print d.getBestFeature(data, [1, 2], True)
+    d.train(data, [1, 2])
+    print d.classify([1, 1])
