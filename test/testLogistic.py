1a2f538f37b0ad002687234348d385f2c8f10c01
intoblack <intoblack86@gmail.com>
2014-12-30 00:12:42 +0800

    添加提升树

--- test/testLogistic.py
+++ test/testLogistic.py
@@ -7,17 +7,18 @@ class Logistic(object):
 
     def train(self, datas, labels, alpha=0.001):
         self.params = [1. for _ in range(len(datas[0]))]
+        self.labels = set(labels)
 
         for i in range(len(datas)):
-            h = self.sigmod(
-                sum([datas[i][j] * self.params[j] for j in range(len(self.params))]))
+            h = self.sigmod(self.classify(datas[i]))
+            #L(a) = error , 已知损失数值 ， 需要求更新权重的
             error = (labels[i] - h)
             for j in range(len(self.params)):
-                self.params[j] += (alpha * self.classify(datas[i]) * error)
+                self.params[j] += (alpha * datas[i][j] * error)
 
-    def classify(self, datas):
-        return sum([datas[i] * self.params[i] for i in range(len(self.params))])
-
+    def classify(self, data):
+        _val = sum([data[i] * self.params[i] for i in range(len(self.params))]) 
+        return min([abs(label - _val  , label) for label in self.labels ])
     def sigmod(self, x):
 
         return 1. / (1 + exp(-x))
@@ -35,7 +36,7 @@ if __name__ == '__main__':
         if x <= y:
             labels.append(1)
         else:
-            labels.append(-1)
+            labels.append(0)
     b = Logistic()
     b.train(data, labels)
-    print b.classify([5, 1])
+    print b.classify([2, 5])
