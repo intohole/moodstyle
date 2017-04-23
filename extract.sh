180140d833a594eba33cdac2625833f5feaad388
intohole <intoblack86@gmail.com>
2017-04-23 23:46:49 +0800

    add ^\s*def -> extract.sh ; remove TrainHmm.py

--- extract.sh
+++ extract.sh
@@ -2,6 +2,7 @@
 
 
 FUNCTION_FILE="README.md"
+PROJECT_NAME="moodstyle"
 
 
 
@@ -14,9 +15,9 @@ echo "" >> ${FUNCTION_FILE}
 echo "" >> ${FUNCTION_FILE}
 cd `dirname $0`
 CWD=`pwd`
-for function_file in `ls ${CWD}/moodstyle/*.py`;do
+for function_file in `ls ${CWD}/${PROJECT_NAME}/*.py`;do
     echo "+ `basename ${function_file}`" >> ${FUNCTION_FILE}
-    cat ${function_file} | grep -En "(^class|def )" | grep -v "__" | grep -v "def _" | sed  's/class /    + /' | sed 's/def /    + /' | sed 's/:$//' | sed 's/\]/\\]/g' | sed 's/\[/\\[/g' | awk -F ":" '{
+    cat ${function_file} | grep -En "(^class|^\s*def )" | grep -v "__" | grep -v "def _" | sed  's/class /    + /' | sed 's/def /    + /' | sed 's/:$//' | sed 's/\]/\\]/g' | sed 's/\[/\\[/g' | awk -F ":" '{
     line_number = $1;
     function_name  = ""
     for(i = 2 ;i <=NF ;i++){
@@ -26,6 +27,6 @@ for function_file in `ls ${CWD}/moodstyle/*.py`;do
             function_name =$i 
         }
     }
-    print function_name"](b2/'`basename ${function_file}`'#L"line_number")" 
+    print function_name"]('${PROJECT_NAME}'/'`basename ${function_file}`'#L"line_number")" 
 }' | sed 's/+ /+ [/'>> ${FUNCTION_FILE} 
 done
