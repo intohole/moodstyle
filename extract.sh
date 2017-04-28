9d4e3cdd081cd4369377a259252bbc7d18599f67
intohole <intoblack86@gmail.com>
2017-04-28 15:22:05 +0800

    fix

--- extract.sh
+++ extract.sh
@@ -17,16 +17,25 @@ cd `dirname $0`
 CWD=`pwd`
 for function_file in `ls ${CWD}/${PROJECT_NAME}/*.py`;do
     echo "+ `basename ${function_file}`" >> ${FUNCTION_FILE}
-    cat ${function_file} | grep -En "(^class|^\s*def )" | grep -v "__" | grep -v "def _" | sed  's/class /    + /' | sed 's/def /    + /' | sed 's/:$//' | sed 's/\]/\\]/g' | sed 's/\[/\\[/g' | awk -F ":" '{
-    line_number = $1;
-    function_name  = ""
-    for(i = 2 ;i <=NF ;i++){
-        if(i != 2){
-            function_name=function_name":"$i
-        }else{
-            function_name =$i 
-        }
-    }
+    cat ${function_file} | 
+	grep -En "(^class|^\s*def )" | 
+#	grep -v "__" | 
+#	grep -v "def _" | 
+	sed  's/class /    + /' |
+	sed 's/def /    + /' |
+	sed 's/:$//' |
+	sed 's/\]/\\]/g' | 
+	sed 's/\[/\\[/g' | 
+	awk -F ":" '{
+    	line_number = $1;
+    	function_name  = ""
+    	for(i = 2 ;i <=NF ;i++){
+        	if(i != 2){
+            	function_name=function_name":"$i
+        	}else{
+            	function_name =$i 
+        	}
+    	}
     print function_name"]('${PROJECT_NAME}'/'`basename ${function_file}`'#L"line_number")" 
 }' | sed 's/+ /+ [/'>> ${FUNCTION_FILE} 
 done
