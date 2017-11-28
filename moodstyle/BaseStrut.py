3f64abad631a2201c3bcd19bc8e64c6b5a3abc77
intohole <intoblack86@gmail.com>
2017-11-28 15:34:25 +0800

    fix basestruct format wrong

--- moodstyle/BaseStrut.py
+++ moodstyle/BaseStrut.py
@@ -72,76 +72,76 @@ class WeightArray(object):
 class Normalization(object):
 
 
-	def __init__(self, *argv , **kw):
-		pass
+    def __init__(self, *argv , **kw):
+        pass
 
 
-	def update(self , value):
-		raise NotImplementedError
+    def update(self , value):
+        raise NotImplementedError
 
 
-	def get_normalization(self ,value ):
-		raise NotImplementedError
+    def get_normalization(self ,value ):
+        raise NotImplementedError
 
 
 
 class MinMax(Normalization):
-	'''
-	类功能：
-		通过最普通的方式，将数据归一化
-	'''
+    '''
+    类功能：
+        通过最普通的方式，将数据归一化
+    '''
 
 
-	def __init__(self , max_value = None , min_value = None ) :
-		'''
-		function
-			init
-		params
-			max_value 最大值 ， 默认值None
-			min_value 最小值 ， 默认值None
-		'''
-		self.max = max_value 
-		self.min = min_value
+    def __init__(self , max_value = None , min_value = None ) :
+        '''
+        function
+            init
+        params
+            max_value 最大值 ， 默认值None
+            min_value 最小值 ， 默认值None
+        '''
+        self.max = max_value 
+        self.min = min_value
 
 
-	def update(self , value):
-		'''
-		function
-			将属性对应更新，获得数据最大值和最小值
-		params
-			value 属性值 ， value 类型(int , long , float)
-		return 
-			None
-		raise
-			当value无法转换为float值，抛出ValueError
-		'''
-		if value is not None:
-			try:
-				value = float(value)
-			except Exception, e:
-				raise ValueError , e 
-			if self.max is None or self.max < value :
-				self.max = value 
-			if self.min is None or self.min > value :
-				self.min = value 
+    def update(self , value):
+        '''
+        function
+            将属性对应更新，获得数据最大值和最小值
+        params
+            value 属性值 ， value 类型(int , long , float)
+        return 
+            None
+        raise
+            当value无法转换为float值，抛出ValueError
+        '''
+        if value is not None:
+            try:
+                value = float(value)
+            except Exception, e:
+                raise ValueError , e 
+            if self.max is None or self.max < value :
+                self.max = value 
+            if self.min is None or self.min > value :
+                self.min = value 
 
 
-	def get_normalization(self , value ):
-		'''
-		function
-			将value 转换为归一化后的值
-		params
-			value 属性值
-		return 
-			[0,1]
-		raise
-			value == None ValueError
-		'''
-		if value is None :
-			raise ValueError 
-		if self.max == self.min:
-			return 1
-		return (float(value) - self.min) / (self.max - self.min )
+    def get_normalization(self , value ):
+        '''
+        function
+            将value 转换为归一化后的值
+        params
+            value 属性值
+        return 
+            [0,1]
+        raise
+            value == None ValueError
+        '''
+        if value is None :
+            raise ValueError 
+        if self.max == self.min:
+            return 1
+        return (float(value) - self.min) / (self.max - self.min )
 
 
 import math 
@@ -150,62 +150,62 @@ class ZScore(Normalization):
 
 
 
-	def __init__(self):
-		self.avg_value = 0 #均值
-		self.variance = 0 #方差均值
+    def __init__(self):
+        self.avg_value = 0 #均值
+        self.variance = 0 #方差均值
 
 
 
-	def update(self , values):
-		'''
-		function
-			计算数据的平均值和平均方差
-		'''
-		self.avg_value = sum(values) / float(len(values))
-		self.variance = math.sqrt(sum( (value - self.avg_value) ** 2  for value in values ) ) 
+    def update(self , values):
+        '''
+        function
+            计算数据的平均值和平均方差
+        '''
+        self.avg_value = sum(values) / float(len(values))
+        self.variance = math.sqrt(sum( (value - self.avg_value) ** 2  for value in values ) ) 
 
 
-	def get_normalization(self , value):
-		if value is None :
-			raise ValueError 
-		if self.avg_value == 0:
-			return 0 
-		return ( float(value)  - self.avg_value ) / self.variance
+    def get_normalization(self , value):
+        if value is None :
+            raise ValueError 
+        if self.avg_value == 0:
+            return 0 
+        return ( float(value)  - self.avg_value ) / self.variance
 
 
 class LogNormalization(Normalization):
 
 
-	def __init__(self , base = 10 ):
-		'''
-		function
-			init
-		params
-			base log基数，最好为最大值
-		return 
-			None 
-		raise 
-			None 
-		'''
-		self.base = base
+    def __init__(self , base = 10 ):
+        '''
+        function
+            init
+        params
+            base log基数，最好为最大值
+        return 
+            None 
+        raise 
+            None 
+        '''
+        self.base = base
 
 
-	def get_normalization(self , value ):
-		if value is None :
-			raise ValueError 
-		return math.log(value , self.base )
+    def get_normalization(self , value ):
+        if value is None :
+            raise ValueError 
+        return math.log(value , self.base )
 
 class Arccotx(Normalization):
 
 
-	def get_normalization(self , value ):
-		if value is None:
-			raise ValueError
-		return math.atan(float(value)) * 2 / math.pi 
+    def get_normalization(self , value ):
+        if value is None:
+            raise ValueError
+        return math.atan(float(value)) * 2 / math.pi 
 
 if __name__ == '__main__':
-	l = Arccotx()
-	print l.get_normalization(10)
+    l = Arccotx()
+    print l.get_normalization(10)
 
 
 
