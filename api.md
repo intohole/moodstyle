7aad5f5829df1cc8d909a13b296dfde37e481732
intohole <intoblack86@gmail.com>
2017-12-03 18:30:12 +0800

    update doc

--- api.md
+++ api.md
@@ -11,3 +11,39 @@ API
         - labels 打标数据,[1,-1]
     - predict
         - data 预测数据，数据形式为[0,1]
+
+
++ moodstyle.Bandit.Greedy 
+    
+    - 参数(Paramters):
+        - e 探测率(explor rate); e 在(in) (0,1)
+        - N 需要探测桶数(the number of explor bucket)
+
+    - 函数(Function):
+        - getIndex()
+            - 获得运行的桶号(get index of bucket)
+        - process(label)
+            - 获得标注 
+
+
++ moodstyle.DDistance
+    - 计算两个数据之间距离(caculate distance of input data)
+    - 函数(Function)
+        - distance(data1,data2)
+            - data1 输入数据(input data); 例如（example）: [1,2]
+            - data2 输入数据(input data); 例如 （example）:[2,3]
+            - warn: length of data1 is equal to the length of data2 
+    - moodstyle.DDistance.Manhattan
+        - 曼哈顿距离
+
+    - moodstyle.DDistance.Chebyshev
+        - 切比雪夫距离
+   
+    - moodstyle.DDistance.Cosine
+        - 余弦距离
+    
+    - moodstyle.DDistance.Hamming
+        - 海明距离
+    
+    - moodstyle.DDistance.Eucliden
+        - 欧式距离 
--- moodstyle/Bandit.py
+++ moodstyle/Bandit.py
@@ -6,6 +6,8 @@ import random
 
 
 class Greedy(object):
+    """
+    """
     EXPLORER = 1
     WORK = 0 
     
@@ -50,38 +52,38 @@ class Greedy(object):
             self._prop()
 
 
-class UCB(object):
-    
-
-    def __init__(self,N,max_value):
-        self.N = N
-        self.max_value = max_value
-        self._count = 0
-        self._sub_count = [0.] * N
-        self._sub_sum = [0.] * N
-        self.p = [0.] * N
-        self._last = None
-    
-
-    def _prop(self):
-        for i in range(self.N):
-            if self._sub_count == 0:
-                continue
-            self.p[i] = self._sub_sum[i] / self._sub_count + math.sqrt( 2 * math.log(self._count) / self._sub_count[i])
-
-    def getIndex(self):
-        for i in range(self.N):
-            if self._sub_count[i] == 0:
-                self._last = i
-                return self._last            
-        self._last = max(enumerate(self.p),key = lambda x:x[1])[0] 
-        return self._last 
-
-    def process(self,label):
-        self._count += 1
-        self._sub_count[self._last] += 1 
-        self._sub_sum[self._last] += label
-
+#class UCB(object):
+#    
+#
+#    def __init__(self,N,max_value):
+#        self.N = N
+#        self.max_value = max_value
+#        self._count = 0
+#        self._sub_count = [0.] * N
+#        self._sub_sum = [0.] * N
+#        self.p = [0.] * N
+#        self._last = None
+#    
+#
+#    def _prop(self):
+#        for i in range(self.N):
+#            if self._sub_count == 0:
+#                continue
+#            self.p[i] = self._sub_sum[i] / self._sub_count + math.sqrt( 2 * math.log(self._count) / self._sub_count[i])
+#
+#    def getIndex(self):
+#        for i in range(self.N):
+#            if self._sub_count[i] == 0:
+#                self._last = i
+#                return self._last            
+#        self._last = max(enumerate(self.p),key = lambda x:x[1])[0] 
+#        return self._last 
+#
+#    def process(self,label):
+#        self._count += 1
+#        self._sub_count[self._last] += 1 
+#        self._sub_sum[self._last] += label
+#
 if __name__ == "__main__":
     
     N = 100 
