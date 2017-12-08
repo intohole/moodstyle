037feaca211dd53d2d6778f790d538d89110c0d6
intohole <intoblack86@gmail.com>
2017-12-08 14:23:48 +0800

    fix kmeans.py

--- moodstyle/cluster/Kmeans.py
+++ moodstyle/cluster/Kmeans.py
@@ -7,56 +7,51 @@ from collections import Counter
 from collections import defaultdict
 from math import sqrt
 import re
-from DDistance import Manhattan
-from DDistance import Hamming
-from DDistance import Cosine
-from DDistance import Euclidean
+import DDistance
 from copy import copy
 import sys
-
-
+from b2 import exceptions2
+from b2 import sort2
 """
 处理数据的格式 [数据1,数据2]
 但是必须要改写 def distance(data1,data2) 数据距离函数
 数据转换格式 {分类:{数据的位置:数据距离}}
 """
 
-class Center(object):
 
-    def __init__(self, label, center_vector, distance_fun=None):
-        if not isinstance(label, (int, long, basestring)):
-            raise TypeError
-        if not isinstance(center_vector, (list, tuple)):
-            raise TypeError
+class Center(object):
+    """
+    """
+
+    def __init__(self,
+                 label,
+                 center_vector,
+                 distance=DDistance.DefaultDistance()):
+        exceptions2.judge_type(label, (int, long, basestring))
+        exceptions2.judge_type(center_vector, (list, tuple))
+        exceptions2.judge_type(distance, DDistance.DDdistance)
         self.label = label
         self.vector = center_vector
-        if distance_fun is None or callable(distance_fun) is False:
-            self.distance_fun = self.default_distance_fun
-        else:
-            self.distance_fun = distance_fun
+        self._distance = distance
 
     def __sub__(self, value):
-        if vector is None:
-            raise TypeError
-        if isinstance(vector):
-            return self.distance_fun(self.vector, vector)
+        exceptions2.judge_null(value)
+        if isinstance(vector, (list, tuple)):
+            return self._distance(self.vector, vector)
         elif isinstance(value, Center):
-            return self.distance_fun(self.vector, value.vector)
-        elif hasattr(value, "vector") and isinstance(getattr(value, "vector"), (list, tuple)):
-            return self.distance_fun(self.vector, value.vector)
+            return self._distance(self.vector, value.vector)
+        elif hasattr(value, "vector") and isinstance(
+                getattr(value, "vector"), (list, tuple)):
+            return self._distance(self.vector, value.vector)
         else:
             raise TypeError
-        return self.distance_fun(self.vector, vector)
 
 
 class Kmeans(object):
-
     def cluster(self, datas, k, iter_count=10000, diff=0.00001):
-        '''
+        """
         函数功能：
                对数据进行聚类 ， 通过kmeans 算法
-
-
         过程：
              随机从数据选出centers （一个随机过程）
              开始迭代
@@ -67,7 +62,7 @@ class Kmeans(object):
                  计算数据label变化比率 ， 如果超出diff设置值 ， 继续下轮迭代
                  否则 ， 跳出循环
             返回数据labels
-        '''
+        """
         centers = self.rand_seed(datas, k)
         center_range = range(len(centers))
         data_range = range(len(datas))
@@ -75,9 +70,8 @@ class Kmeans(object):
         for _ in range(iter_count):
             diff_labels = 0
             for i in data_range:
-                bestlabel = min(
-                    [(self.distance(datas[i], centers[j][0]), centers[j][1])
-                     for j in center_range])
+                bestlabel = min([(self.distance(datas[i], centers[j][0]),
+                                  centers[j][1]) for j in center_range])
                 if labels[i] != bestlabel[1]:
                     diff_labels += 1
                     labels[i] = bestlabel[1]
@@ -93,7 +87,9 @@ class Kmeans(object):
 
     def update_centers(self, datas, labels, centers):
         centers_dict = {
-            center[1]: [0 for i in range(len(center[0]))] for center in centers}
+            center[1]: [0 for i in range(len(center[0]))]
+            for center in centers
+        }
         label_dict = Counter(labels)
         for i in range(len(datas)):
             for j in range(len(datas[i])):
@@ -101,32 +97,7 @@ class Kmeans(object):
         for label in label_dict.keys():
             for i in range(len(centers_dict[label])):
                 centers_dict[label][i] /= label_dict[label]
-        return sorted([(center, label) for label, center in centers_dict.items()], key=lambda x: x[1], reverse=False)
-
-
-class DKmeans(Kmeans):
-    def distance(self, data1, data2):
-        return sqrt(
-            sum([
-                (data1[i] - data2[i]) ** 2
-                for i in range(len(data1))
-            ])
-        )
-
-class ManhattanKmeans(Kmeans, Manhattan):
-    pass
-
-
-class HammingKmeans(Kmeans, Hamming):
-
-    pass
-
-
-class CosineKmeans(Kmeans, Cosine):
-    pass
-
-
-class EuclideanKmeans(Kmeans, Euclidean):
-    pass
-
-
+        return sorted(
+            [(center, label) for label, center in centers_dict.items()],
+            key=lambda x: x[1],
+            reverse=False)
